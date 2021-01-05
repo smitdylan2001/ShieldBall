ca339bc7c3448b5c6ab0977a8014397a8be552e7
Dylan Smit <dylan.smit01@gmail.com>
2021-01-05 20:05:56 +0100

    fixed shield

--- Assets/Script/Shield.cs
+++ Assets/Script/Shield.cs
@@ -7,6 +7,7 @@ public class Shield : MonoBehaviour
     [SerializeField] private GameObject _shieldGO;
     [SerializeField] private GameObject _rightHand;
     [SerializeField] private GameObject _leftHand;
+    [SerializeField] private GameObject test;
 
     private Ball _ballScript;
 
@@ -20,20 +21,21 @@ public class Shield : MonoBehaviour
         _ballScript = ball;
 
         //_shieldGO = Resources.Load<GameObject>("Shield.prefab"); //TODO: Implement prefab in resources
-
+        test = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        
         //_shieldGO.AddComponent<this>();
         return _shieldGO;
     }
 
 	private void Update()
 	{
-        _shieldGO.transform.position = _leftHand.transform.position;
-        _shieldGO.transform.LookAt(_rightHand.transform);
-        float value = (_leftHand.transform.rotation.eulerAngles.x + _rightHand.transform.rotation.eulerAngles.x) / 2;
-        //Debug.Log(value);
-        Quaternion q = Quaternion.Euler(_shieldGO.transform.rotation.eulerAngles.x + 90 - value, _shieldGO.transform.rotation.eulerAngles.y + 90, _shieldGO.transform.rotation.eulerAngles.z);
+		_shieldGO.transform.position = _leftHand.transform.position;
+		_shieldGO.transform.LookAt(_rightHand.transform);
+		float value = ((_rightHand.transform.forward + _rightHand.transform.position +_leftHand.transform.forward + _leftHand.transform.position) / 2).y / (Mathf.PI/180); //Go to radtodeg function
+        //Quaternion q = Quaternion.Euler(_shieldGO.transform.rotation.eulerAngles.x + value, _shieldGO.transform.rotation.eulerAngles.y + 90, _shieldGO.transform.rotation.eulerAngles.z);
+        Quaternion q = Quaternion.Euler(0 + value, _shieldGO.transform.rotation.eulerAngles.y + 90, _shieldGO.transform.rotation.eulerAngles.z);
         _shieldGO.transform.rotation = q;
-
+        
         _handsMiddle = (_rightHand.transform.position + _leftHand.transform.position) / 2;
         _handsDistance = Vector3.Distance(_rightHand.transform.position, _leftHand.transform.position);
         _handsDistanceVector = new Vector3(Mathf.Abs(_leftHand.transform.localPosition.x - _rightHand.transform.localPosition.x), 
@@ -48,7 +50,7 @@ public class Shield : MonoBehaviour
 		if(collision.gameObject.CompareTag("Ball"))
         {
             //_ballScript.GetBounced(_handsDistance, _shieldGO.transform.forward);
-            EventManager<Shield>.InvokeEvent(EventType.ON_BALL_HIT, this);
+            //EventManager<Shield>.InvokeEvent(EventType.ON_BALL_HIT, this);
             EventManager.InvokeEvent(EventType.ON_BALL_HIT);
         }
 	}
